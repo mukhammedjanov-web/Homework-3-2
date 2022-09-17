ef9380772e3c2c0e9ca25f1c95118ab6166d7a79
mukhammedjanov-web <akmalmuhammadjonov6@gmail.com>
2022-09-17 19:27:00 +0500

    Month-3 lesson-3

---
+++ script.js
@@ -0,0 +1,311 @@
+"use strict"
+// 3-dars mashqlari
+
+//  1-m
+// let A=6
+
+// if (A>0){
+//     console.log(`${A} soni musbat`);
+// }else  console.log(`${A} soni manfiy`);
+
+
+//  2-m
+// let A=7
+
+// if (A%2===1){
+//     console.log(`${A} toq son`);
+// }else console.log(`${A} toq son emas`);
+
+
+//  3-m
+// let A=8
+
+// if (A%2===0){
+//     console.log(`${A} juft son`);
+// }else console.log(`${A} juft son emas`);
+
+
+//  4-m
+// let A=8
+// let B=10
+
+// if (A>=0 && B>=0){
+//     console.log(`Butun son`);
+// }else console.log(`Butun son emas`);
+
+
+//  5-m
+// let a=15
+// let b=10
+// let c=8
+
+// if (a>b && b>c){
+//     console.log(`True`);
+// }else console.log(`False`);
+
+
+// //  6-m
+// let a=15
+// let b=16
+// let c=24
+
+// if (b>a && b<c || b>c && b<a){
+//     console.log(`True`);
+// }else console.log(`False`);
+
+
+//  7-m
+// let a=15
+// let b=16
+
+
+// if (a%2===1 && b%2===1){
+//     console.log(`True`);
+// }else console.log(`False`);
+
+
+//  8-m
+// let a=15
+// let b=16
+// let c=19
+// let d=25
+
+
+// if (a>0 && b>0 && c>0 && d>0){
+//     console.log(`True`);
+// }else console.log(`False`);
+
+
+//  9-m
+// let a=15
+// let b=16
+// let c=19
+
+
+// if (a>0 && b>0 || a>0 && c>0 || b>0 && c>0){
+//     console.log(`True`);
+// }else console.log(`False`);
+
+
+//  10-m
+// let a=7
+
+// switch (a) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+//     case 5:
+//         console.log("Thursday");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("Saturday");
+//         break;
+//     default:  console.log("False");
+//         break;
+// }
+
+
+//  11-m
+// let a=90
+
+// if (a==90 || a==91){
+//     console.log(`Beeline`);
+// }
+// else if (a==93 ||a==94){
+//     console.log(`Ucell`);
+// }
+// else if (a==97 ||a==88){
+//     console.log(`MobiUz`);
+// }
+// else if (a==33 ||a==99){
+//     console.log(`Uzmobile`);
+
+// } else console.log(`Bunday kodli kompaniya yo'q`);
+
+
+//  12-m
+// let a=-90
+
+// if (a>0){
+//     console.log(`${a+1}`);
+// } else console.log(`${a-1}`);
+
+
+//  13-m
+// let a=-90
+
+// if (a>0){
+//     console.log(`${a+3}`);
+// } else console.log(`${a-2}`);
+
+
+//  14-m
+// let a=90;
+// let b=95;
+
+// if (a>b){
+//     console.log(`${a} soni ${b} sonidan katta`);
+// } else  console.log(`${b} soni ${a} sonidan katta`);
+
+
+//  15-m
+// let a=90;
+// let b=90;
+// let c=90;
+
+// if (a>b && a>c){
+//     console.log(`${a} soni ${b} va ${c} sonidan katta`);
+// }
+// else if (b>a && b>c){
+//     console.log(`${b} soni ${a} va ${c} sonidan katta`);
+// }
+// else if (c>a && c>b){
+//     console.log(`${c} soni ${a} va ${b} sonidan katta`);
+// } else  console.log(`False`)
+
+
+//  16-m
+// let a=90;
+// let b=901;
+// let c=90;
+
+// if (a<b && a<c){
+//     console.log(`${a} soni ${b} va ${c} sonidan kichik`);
+// }
+// else if (b<a && b<c){
+//     console.log(`${b} soni ${a} va ${c} sonidan kichik`);
+// }
+// else if (c<a && c<b){
+//     console.log(`${c} soni ${a} va ${b} sonidan kichik`);
+// } else  console.log(`False`)
+
+
+//  18-m
+// let a=5;
+
+// if (a==3){
+//     console.log(`Sizga 300 ming so'm stipendiya beriladi`);
+// }
+// else if (a==4){
+//     console.log(`Sizga 400 ming so'm stipendiya beriladi`);
+// }
+// else if (a==5){
+//     console.log(`Sizga 500 ming so'm stipendiya beriladi`);
+// } else  console.log(`Bunday baho yo'q`)
+
+
+//  19-m
+// let a=6;
+
+// if (a>0 && a%2==1){
+//     console.log(`${a} toq musbat son`);
+// }
+// else if (a>0 && a%2==0){
+//     console.log(`${a} juft musbat son`);
+// }
+// else if (a<0 && a%2==-1){
+//     console.log(`${a} toq manfiy son`);
+// }
+// else if (a<0 && a%2==0){
+//     console.log(`${a} juft manfiy son`);
+// }
+//  else  console.log(`Butun son emas`)
+
+
+//  20-m
+// let a=2010;
+
+// if (a<2022 && a>0){
+//     console.log(`${2022-a} yoshdasiz`);
+// } else  console.log(`False`)
+
+
+// //  21-m
+// let a=1;
+// let b=2;
+// let c=6;
+
+// if (a<=b && b<=c){
+//     console.log(`True`);
+// } else  console.log(`False`)
+
+
+//  22-m
+// let a=3;
+// let b=3;
+
+// if ((a%2==1 && b%2==0) || (b%2==1 && a%2==0)){
+//     console.log(`True`);
+// }
+//  else  console.log(`False`)
+
+
+//  23-m
+// let a=-3;
+// let b=5;
+// let c=6;
+
+// if (a>0 && b>0 && c>0){
+//     console.log(`True`);
+// } else  console.log(`False`)
+
+
+//  24-m
+// let a=-3;
+// let b=5;
+// let c=-6;
+
+// if ((a>0 && b<0 && c<0) || (a<0 && b>0 && c<0) || (a<0 && b<0 && c>0)){
+//     console.log(`True`);
+// } else  console.log(`False`)
+
+
+//  25-m
+// let a=156;
+// let b=Math.floor(a/100);
+// let c=Math.floor((a%100)/10);
+// let d=Math.floor(a%10);
+
+
+// if (b!=c && c!=d && b!=d){
+//     console.log(`True`);
+// } else  console.log(`False`)
+
+
+//  26-m
+// let a=123;
+// let b=Math.floor(a/100);
+// let c=Math.floor((a%100)/10);
+// let d=Math.floor(a%10);
+
+
+// if ((b<c && c<d) && (c-b==1 && d-c==1)){
+//     console.log(`True`);
+// } else  console.log(`False`)
+
+
+//  27-m
+// let a=prompt ("Komyuter nomini kiriting");
+
+
+// if (a=="lenovo" || a=="Lenovo" || a=="LENOVO"){
+//     console.log(`2 000 000  so'm`);
+// } else if(a=="acer" || a=="Acer" || a=="ACER"){
+//     console.log(`3 000 000  so'm`);
+// } else if(a=="asus" || a=="Asus" || a=="ASUS"){
+//     console.log(`4 000 000  so'm`);
+// } else  console.log(`Bunday kompyuter yo'q`)
+
+
